fbc99cf374b6056688f4536754d20a398dcd22d0
Joost Baars <25607916+JoostBaars@users.noreply.github.com>
2022-11-25 11:37:33 +0100

    Added the newline for the AddRow() function in file_writer

--- src/exports/file_writer.cpp
+++ src/exports/file_writer.cpp
@@ -5,11 +5,17 @@ FileWriter::FileWriter(const std::string &fileName) : file_{}
   file_.open(fileName.c_str(), std::ios::trunc | std::ios::out);
 }
 
+/**
+ * @brief adds a row in the file
+ *
+ * @return true no errors occured
+ * @return false could not add the text in the file, file corrupt
+ */
 bool FileWriter::AddRow(const std::string &text)
 {
   if (file_.is_open())
   {
-    file_ << text;
+    file_ << text << "\n";
     return true;
   }
   return false;
